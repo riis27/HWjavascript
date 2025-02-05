79a7a3d21faa724ed33bc88b48208f82fd8ec8b1
rissa <rissasanmiguel@gmail.com>
2025-02-05 01:00:23 -0500

    hw.js

---
+++ app.js
@@ -0,0 +1,23 @@
+var Apple = 10;
+var Pear = 5;
+
+function functionwithArgs(Apple,Pear) {
+    console.log(Apple + Pear)
+  }
+  functionwithArgs(10,5)
+
+
+  function functionwithArgs(Apple,Pear) {
+    console.log(Apple - Pear)
+  }
+  functionwithArgs(10,5)
+
+  function functionwithArgs(Apple,Pear) {
+    console.log(Apple * Pear)
+  }
+  functionwithArgs(10,5)
+
+  function functionwithArgs(Apple,Pear) {
+    console.log(Apple / Pear)
+  }
+  functionwithArgs(10,5)
