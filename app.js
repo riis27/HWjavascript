4bce06abd39db0b53c3174e7ecdd947a20c92794
rissa <rissasanmiguel@gmail.com>
2025-02-10 01:46:45 -0500

    js functions hw

--- app.js
+++ app.js
@@ -1,23 +1,67 @@
-var Apple = 10;
-var Pear = 5;
+//2
+var Kendrick = 11;
+var Lamar = 12;
+sumArg = Kendrick + Lamar;
 
-function functionwithArgs(Apple,Pear) {
-    console.log(Apple + Pear)
-  }
-  functionwithArgs(10,5)
+function sumArg() {
+    return Kendrick + Lamar;
+}
+
+console.log(sumArg)
 
 
-  function functionwithArgs(Apple,Pear) {
-    console.log(Apple - Pear)
-  }
-  functionwithArgs(10,5)
+//4
+var Kendrick = 11;
+var Lamar = 12;
+difArg = Kendrick - Lamar;
 
-  function functionwithArgs(Apple,Pear) {
-    console.log(Apple * Pear)
-  }
-  functionwithArgs(10,5)
+function difArg() {
+    return Kendrick - Lamar;
+}
 
-  function functionwithArgs(Apple,Pear) {
-    console.log(Apple / Pear)
-  }
-  functionwithArgs(10,5)
+console.log(difArg)
+
+//5
+var Kendrick = 11;
+var Lamar = 12;
+multiArg = Kendrick * Lamar;
+
+function multiArg() {
+    return Kendrick * Lamar;
+}
+
+console.log(multiArg)
+
+//6
+var Kendrick = 11;
+var Lamar = 12;
+difArg = Kendrick / Lamar;
+
+function difArg() {
+    return Kendrick / Lamar;
+}
+
+console.log(difArg)
+
+//7
+
+var Kendrick = 11;
+var Lamar = 12;
+
+// 1. sumArg (Addition)
+const sumArg = (num1, num2) => num1 + num2;
+console.log(sumArg(Kendrick, Lamar));
+
+// 2. difArg (Subtraction)
+const difArg = (num1, num2) => num1 - num2;
+console.log(difArg(Kendrick, Lamar));
+
+// 3. multiArg (Multiplication)
+const multiArg = (num1, num2) => num1 * num2;
+console.log(multiArg(Kendrick, Lamar));
+
+// 4. divArg (Division)  (Corrected variable name to avoid conflict)
+const divArg = (num1, num2) => num1 / num2; // Corrected function name
+console.log(divArg(Kendrick, Lamar));
+
+
