8d1c8c7d5ac327a1260fad73243f22d29c8c94ea
rissa <rissasanmiguel@gmail.com>
2025-02-13 00:34:58 -0500

    hw 2.12.25

--- app.js
+++ app.js
@@ -1,67 +1,59 @@
-//2
-let Kendrick = 11;
-let Lamar = 12;
-sumArg = Kendrick + Lamar;
+//2.12.25
 
-function sumArg() {
-    return Kendrick + Lamar;
-}
+//3.
+const myArray = [["Star", 11],["Moon", 12]["Sun", 13]];
 
-console.log(sumArg)
-
-
-//4
-let Kendrick = 11;
-let Lamar = 12;
-difArg = Kendrick - Lamar;
-
-function difArg() {
-    return Kendrick - Lamar;
-}
-
-console.log(difArg)
-
-//5
-let Kendrick = 11;
-let Lamar = 12;
-multiArg = Kendrick * Lamar;
-
-function multiArg() {
-    return Kendrick * Lamar;
-}
-
-console.log(multiArg)
-
-//6
-let Kendrick = 11;
-let Lamar = 12;
-divArg = Kendrick / Lamar;
-
-function divArg() {
-    return Kendrick / Lamar;
-}
-
-console.log(divArg)
-
-//7
-
-let Kendrick = 11;
-let Lamar = 12;
-
-// 1. sumArg (Addition)
-const sumArg = (num1, num2) => num1 + num2;
-console.log(sumArg(Kendrick, Lamar));
-
-// 2. difArg (Subtraction)
-const difArg = (num1, num2) => num1 - num2;
-console.log(difArg(Kendrick, Lamar));
-
-// 3. multiArg (Multiplication)
-const multiArg = (num1, num2) => num1 * num2;
-console.log(multiArg(Kendrick, Lamar));
-
-// 4. divArg (Division)  
-const divArg = (num1, num2) => num1 / num2;
-console.log(divArg(Kendrick, Lamar));
+//4.
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+        product *= arr[i][j];
+      }
+    }
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 
 
+  //5.
+  function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val > 10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan(10);
+
+  //6.
+  function twoArgs(val1, val2){
+    if(val1 === val2){
+        return 'equal';
+    }
+    if(val1 !== val2){
+        return 'different';
+    }
+  }
+  console.log(twoArgs)
+  twoArgs(2,6)
+  
+  
+  function twoArgs(val1, val2){
+    if(val1 === val2){
+        return 'equal';
+    }
+    if(val1 !== val2){
+        return 'different';
+    }
+  }
+  console.log(twoArgs)
+  twoArgs(6,6)
